d550b0db63fa36bd364466bbef53de656581d8bc
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-11 15:18:45 +0100

    feat: added endpoint routes

---
+++ api/controllers/routes.go
@@ -0,0 +1,26 @@
+package controllers
+
+import "github.com/Arka-cell/goassignment/api/middlewares"
+
+func (s *Server) initializeRoutes() {
+
+	// Home Route
+	s.Router.HandleFunc("/", middlewares.SetMiddlewareJSON(s.Home)).Methods("GET")
+
+	// Login Route
+	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(s.Login)).Methods("POST")
+
+	//Users routes
+	s.Router.HandleFunc("/signup", middlewares.SetMiddlewareJSON(s.CreateShop)).Methods("POST")
+	s.Router.HandleFunc("/shops", middlewares.SetMiddlewareJSON(s.GetShops)).Methods("GET")
+	s.Router.HandleFunc("/shops/{id}", middlewares.SetMiddlewareJSON(s.GetShop)).Methods("GET")
+	s.Router.HandleFunc("/shops/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateShop))).Methods("PUT")
+	s.Router.HandleFunc("/shops/{id}", middlewares.SetMiddlewareAuthentication(s.DeleteShop)).Methods("DELETE")
+
+	//Posts routes
+	s.Router.HandleFunc("/products", middlewares.SetMiddlewareJSON(s.CreateProduct)).Methods("POST")
+	s.Router.HandleFunc("/products", middlewares.SetMiddlewareJSON(s.GetProducts)).Methods("GET")
+	s.Router.HandleFunc("/products/{id}", middlewares.SetMiddlewareJSON(s.GetProduct)).Methods("GET")
+	s.Router.HandleFunc("/products/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateProduct))).Methods("PUT")
+	s.Router.HandleFunc("/products/{id}", middlewares.SetMiddlewareAuthentication(s.DeleteProduct)).Methods("DELETE")
+}
