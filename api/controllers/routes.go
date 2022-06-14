f4b12d85cdb3e36a89e6ad538c7222a56131e5c2
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-14 14:48:42 +0100

    docs: improved comments

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -10,14 +10,14 @@ func (s *Server) initializeRoutes() {
 	// Login Route
 	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(s.Login)).Methods("POST")
 
-	//Users routes
+	//Shops routes
 	s.Router.HandleFunc("/signup", middlewares.SetMiddlewareJSON(s.CreateShop)).Methods("POST")
 	s.Router.HandleFunc("/shops", middlewares.SetMiddlewareJSON(s.GetShops)).Methods("GET")
 	s.Router.HandleFunc("/shops/{id}", middlewares.SetMiddlewareJSON(s.GetShop)).Methods("GET")
 	s.Router.HandleFunc("/shops/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateShop))).Methods("PUT")
 	s.Router.HandleFunc("/shops/{id}", middlewares.SetMiddlewareAuthentication(s.DeleteShop)).Methods("DELETE")
 
-	//Posts routes
+	//Products routes
 	s.Router.HandleFunc("/products", middlewares.SetMiddlewareJSON(s.CreateProduct)).Methods("POST")
 	s.Router.HandleFunc("/products", middlewares.SetMiddlewareJSON(s.GetProducts)).Methods("GET")
 	s.Router.HandleFunc("/products/{id}", middlewares.SetMiddlewareJSON(s.GetProduct)).Methods("GET")
